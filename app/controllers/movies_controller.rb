b1549fdb72c5c8e9f7343ea659429c982cc364d9
Annie Shaw <as6479@columbia.edu>
2022-10-05 02:59:47 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,33 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    redirect = false
+
+    if params[:ratings]
+      session[:ratings] = params[:ratings]
+    else
+      redirect = true
+    end
+    session[:ratings] = session[:ratings] || Hash[ @all_ratings.map {|ratings| [ratings, 1]} ]
+    @ratings = session[:ratings]
+
+    if params[:category]
+      session[:category] = params[:category]
+    else
+      redirect = true
+    end
+    session[:category] = session[:category] || ""
+    @category = session[:category]
+
+    if redirect
+      flash.keep
+      redirect_to movies_path({:category => @category, :ratings => @ratings})
+    end
+
+    @movies = Movie.where("rating in (?)", @ratings.keys).find(:all, :order => @category)
+
   end
 
   def new
@@ -36,6 +62,15 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+
+  def same_director
+    movie = Movie.find(params[:id])
+    redirect_to movies_path if movie.director.empty?
+
+    flash[:notice] = "'#{movie.title}' has no director info"
+    @movies = movie.same_director
+    redirect_to movies_path if @movies.empty?
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
-  
+  resources :movies do
+    get :same_director, on: :member
+  end  
   # Example resource route with options:
   #   resources :products do
   #     member do
